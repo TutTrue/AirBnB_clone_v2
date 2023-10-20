7414d311cd52e377b8db56459329904bc9e2a7c9
MahmoudHamdy <hamdy.xd55@gmail.com>
2023-10-20 15:03:57 +0300

    number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    """display hello world"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """display HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return f'C {text.replace("_", " ")}'
+
+
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    return f'Python {text.replace("_", " ")}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f'{n:d} is a numebr'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
