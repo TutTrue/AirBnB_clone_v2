8a12221dfaa617502f2a21c44ba77db9038e5c14
MahmoudHamdy <hamdy.xd55@gmail.com>
2023-10-21 13:22:24 +0300

    fix states template

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """script to fetch states list from storage"""
-from flask import Flask
+from flask import Flask,render_template 
 from models import storage
 from models.state import State
 
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown():
+def teardown(exc):
     """close the session after each request"""
     storage.close()
 
@@ -17,4 +17,8 @@ def teardown():
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """get state list from storage"""
-    return render_tempelete("7-states_list.htm", states=storage.all(Staet))
+    return render_template("7-states_list.html", states=storage.all(State))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
