1a7cf383435297187ba82b475bbb9724b1896f92
MahmoudHamdy <hamdy.xd55@gmail.com>
2023-10-21 13:25:36 +0300

    fix states template and pycodestyle

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """script to fetch states list from storage"""
-from flask import Flask,render_template 
+from flask import Flask, render_template
 from models import storage
 from models.state import State
 
