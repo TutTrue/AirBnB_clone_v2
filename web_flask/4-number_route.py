7c4c91ebd04b4bc0f3e22b52588d6e63cddea1f9
MahmoudHadmy <hamdy.xd55@gmail.com>
2023-10-21 07:36:37 +0300

    number route

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,42 +1,41 @@
 #!/usr/bin/python3
-"""flask web application"""
-
+"""start a flack server listening on port 5000 handling '/'"""
 from flask import Flask
 from markupsafe import escape
-
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_HBNB():
-    """display hello world"""
-    return 'Hello HBNB!'
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """handling / route"""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
-def HBNB():
-    """display HBNB"""
-    return 'HBNB'
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """handling hbnb route"""
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """display text"""
-    return f'C {text.replace("_", " ")}'
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """handling varible routes"""
+    return f"C {escape(text.replace('_', ' '))}"
 
 
-@app.route('/python/')
-@app.route('/python/<text>', strict_slashes=False)
-def python(text='is cool'):
-    """display text"""
-    return f'Python {text.replace("_", " ")}'
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is cool"):
+    """handling varible routes"""
+    return f"Python {escape(text.replace('_', ' '))}"
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    """display number"""
-    return f'{escape(n)} is a numebr'
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_a_number(n):
+    """handling numbers only"""
+    return f"{escape(n)} is a number"
 
 
 if __name__ == '__main__':
+    """start the server"""
     app.run(host='0.0.0.0', port=5000)
