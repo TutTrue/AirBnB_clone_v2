9907c15bf1c5606f4c0b663e6bb4a2993d266096
MahmoudHamdy <hamdy.xd55@gmail.com>
2023-10-21 10:54:14 +0300

    refactor state getter

--- models/state.py
+++ models/state.py
@@ -11,17 +11,16 @@ import os
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state')
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state')
 
     if os.getenv('HBNB_TYPE_STORAGE') != 'db':
-        name = ''
         @property
         def cities(self):
-            """ Getter for Cities of the State"""
-            st_cities = []
-            for city in self.cities:
-                if city.state_id == self.id:
-                    st_cities.append(city)
-            return st_cities
+            """rGetter for Cities of the State"""
+            cities_instances = []
+            cities_dict = models.storage.all(City)
+            for key, value in cities_dict.items():
+                if self.id == value.state_id:
+                    cities_instances.append(value)
+            return cities_instances
